60c844d979cbe502d54300fd26065c460659be27
SanjayDutta <sanjaydutta976@gmail.com>
2024-12-30 12:54:06 +0530

    nav section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Hero, Highlights, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Highlights = () => {
+  return <div>Highloghts</div>;
+};
+
+export default Highlights;
